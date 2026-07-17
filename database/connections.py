f14fb58a074458c2a2c5dd67b46ecc0b88d1e7e8
Krish Verma <krishverma6797@gmail.com>
2026-07-17 16:59:09 +0530

    Update connections.py

--- database/connections.py
+++ database/connections.py
@@ -2,12 +2,18 @@ import os
 import pymysql
 
 def get_connection():
+    print("HOST:", os.getenv("DB_HOST"))
+    print("PORT:", os.getenv("DB_PORT"))
+    print("USER:", os.getenv("DB_USER"))
+    print("DB:", os.getenv("DB_NAME"))
+
     return pymysql.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT")),
         ssl={"ssl": {}},
-        cursorclass=pymysql.cursors.DictCursor
+        cursorclass=pymysql.cursors.DictCursor,
+        connect_timeout=10,
     )
